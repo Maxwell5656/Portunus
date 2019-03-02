eb6e8e396afd3985d611688559f177a63bde95f2
Maxwell5656 <marvin.martian2009@gmail.com>
2019-03-01 18:33:09 -0600

    This is my project. There are many like it, but this one is mine,

---
+++ src/portunus/StringConcater.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package portunus;
+
+/**
+ *
+ * @author Maxwell
+ */
+public class StringConcater {
+    private String Ident;
+    private String Password;
+    private String Username;
+    private String[] SecQuestions;
+    private String[] SecAnswers;
+    // COMMIT MY CHANGES
+}

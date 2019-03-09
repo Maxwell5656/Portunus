8512f4bc0f30709160ec9417a57191a9758d4331
paddy <paddy@DESKTOP-4LV4VP3>
2019-03-08 18:41:03 -0600

    Added Framework for Hash Class

---
+++ src/portunus/HashingFunction.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package portunus;
+
+/**
+ *
+ * @author paddy
+ */
+public class HashingFunction {
+    //TODO: Write Hash Function
+    
+}

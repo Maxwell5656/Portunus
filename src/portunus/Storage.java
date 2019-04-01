37d37fc7790e43605971c02de8ba97af5bdc3d93
Maxwell5656 <marvin.martian2009@gmail.com>
2019-03-31 19:39:53 -0500

    Started work on the Storage Class (see draw.io for current plan)

--- src/portunus/Storage.java
+++ src/portunus/Storage.java
@@ -5,11 +5,26 @@
  */
 package portunus;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.File;
+import java.io.BufferedWriter;
+import javax.crypto.Cipher;
 /**
  *
  * @author Maxwell
  */
 public class Storage {
     //TODO: Make Storage Class
+    private BufferedReader fileIn;
+    private BufferedWriter fileOut;
     
+    
+    public Storage(String fileName)
+    // Note this is designed for files stored in User.dir. This location can be found in Help->About in Netbeans -Maxwell
+    {
+        String fileLocation = System.getProperty("user.dir") + fileName;
+        
+        //fileIn = new BufferedReader(new FileReader());
+    }
 }

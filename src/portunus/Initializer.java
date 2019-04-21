09f5058e4895aeac87d2ac1818d15ab904a4778a
Maxwell5656 <marvin.martian2009@gmail.com>
2019-04-21 10:11:20 -0500

    Changed interface to allow for fixed buttons

--- src/portunus/Initializer.java
+++ src/portunus/Initializer.java
@@ -21,10 +21,9 @@ public class Initializer {
     public static void main(String[] args) {
         // TODO code application logic here
         Info info = new Info();
-        Interface view = new Interface();
-        view.addAccountCreator(new AccountCreator(info));
-        info.addObserver(new InfoToView(info, view));
-        //view.display(args, new AccountCreator(info));
+        //Interface view = new Interface();
+        //view.addAccountCreator(new AccountCreator(info));
+        //view.display(args, new AccountCreator(infoo));
         /*java.awt.EventQueue.invokeLater(new Runnable() {
                 public void run() {
                     view.setVisible(true);
@@ -32,6 +31,39 @@ public class Initializer {
                 }
             });*/
         
-        Interface.display(view);
+        //Interface.display(view);
+        
+        // in this code, the interface is created
+        // note how the interface creation is inside of run()
+        // it seems that this is needed for the buttons to display the correct look and feel of the buttons
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo infoo : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(infoo.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(infoo.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Interface.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Interface.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Interface.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Interface.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        //</editor-fold>
+
+        /* Create and display the form */
+            java.awt.EventQueue.invokeLater(new Runnable() {
+                public void run() {
+                    Interface view = new Interface();
+                    view.addAccountCreator(new AccountCreator(info));
+                    info.addObserver(new InfoToView(info, view));
+                    view.setVisible(true);
+                    
+                }
+            });
     }
 }
